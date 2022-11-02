1d123a5dc0812e8e98313c7511c841ffeb700dc9
myunusyesil <mehmetyunus@windowslive.com>
2022-11-02 15:52:42 +0300

    improv

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
 
-// declare an array 
+// declare a sample array 
 const numArr = [42,29,55,1,2,6,3,4,5,22,56,23,8,19];
+// findPeaks(numArr);
 
 // finding multiple peaks
 function findPeaks(arr) {
     // uçları dahil etmek için
     arr[-1]  = -Infinity;
     arr[arr.length] = -Infinity;
+    //////////////
+
     let peaks = [];
     
     for (let i = 0; i < arr.length-1; i++) {
@@ -18,12 +21,12 @@ function findPeaks(arr) {
     console.log("The peaks of the dataset: " + peaks);
     } 
 
-
     async function readCSV() {
         
-        const indexArr = [];
-        const heightArr = [];
-        const weightArr = [];
+        // define colomn arrays
+        const x1 = [];
+        const x2 = [];
+        const x3 = [];
 
         let url = 'hw_200.csv';
         const response = await fetch(url);
@@ -39,31 +42,31 @@ function findPeaks(arr) {
             column[1] = parseFloat(column[1]);
             column[2] = parseFloat(column[2]);
 
-            indexArr.push(column[0]);
-            heightArr.push(column[1]);
-            weightArr.push(column[2]);
+            x1.push(column[0]);   // 1. colomn
+            x2.push(column[1]);  // 2. colomn
+            x3.push(column[2]);  // 3. colomn
         });
         // console.log(heightArr);
         // findPeaks(heightArr); // when the data is ready we can find peaks via func
-        // console.log("Finished fetching the data")
-         // we need to update chart when we fetch the data
-
-        return {indexArr, heightArr, weightArr};
+         
+        return {x1, x2, x3};
     }
 
-
     async function chartIt() {
+
+    // calling csv data to initialize chart will use it on
     const testData = await readCSV();
+    // console.log("Finished fetching the data")
     // console.log(testData)
 
     const ctx = document.querySelector('#myChart').getContext('2d');
     const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: testData.indexArr,
+        labels: testData.x1,
         datasets: [{
-            label: 'Weight',
-            data: testData.weightArr,
+            label: 'Weight (pounds)',
+            data: testData.x2,
             backgroundColor: [
                 'rgba(250, 0, 0, 0.4)'
             ],
@@ -73,8 +76,8 @@ function findPeaks(arr) {
             borderWidth: 1
         },
         {
-            label: 'Height',
-            data: testData.heightArr,
+            label: 'Height (inch)',
+            data: testData.x3,
             backgroundColor: [
                 'rgba(0, 0, 0, 0.4)'
             ],
@@ -91,6 +94,7 @@ function findPeaks(arr) {
             }
         },
         responsive: true,
+        tension: 0.5
         
     }
 });
